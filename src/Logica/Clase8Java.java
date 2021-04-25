314b0b18cd775c69a3903bbb28cf3a92f2c764fd
Natalia <gonzalezsolisnm@gmail.com>
2021-04-25 15:51:37 -0300

    mi primera interfaz grafica de java

---
+++ src/Logica/Clase8Java.java
@@ -0,0 +1,16 @@
+package Logica;
+
+import IGU.Principal;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Principal princi = new Principal(); // creo mi pantalla, para que me lo traiga del IGU
+        princi.setVisible(true); //para que sea visible 
+        princi.setLocationRelativeTo(null); // para que la centre
+    }
+    
+}
